8a471420937f71eba13aa274a825dd0e70acd1e5
Arianahorn <74698341+Arianahorn@users.noreply.github.com>
2021-02-16 20:02:46 -0800

    scroll back up

--- homepage.js
+++
@@ -1,8 +0,0 @@
-
-// Function for Header and Footer to load
-// $(function(){
-//     $("#header").load("header.html"); 
-//     $("#footer").load("footer.html"); 
-// });
-
-// document.getElementById("id").innerHTML='<object type="text/html" data="x.html"></object>';
